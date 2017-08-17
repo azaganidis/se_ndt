dbac442fba6d1fcbdafd657b59f033ba2d653be5
azaganidis <anestis@ira1908>
2017-08-17 16:33:00 +0100

    Added utility for concatenation of transforms

--- src/concat_transform.cpp
+++ src/concat_transform.cpp
@@ -8,9 +8,8 @@
 
 using namespace std;
 namespace po = boost::program_options;
-Eigen::Affine3d getTrans(string filename)
+Eigen::Affine3d getTrans(istream &infile)
 {
-	ifstream infile(filename); // for example
 	string line = "";
 	Eigen::Affine3d T;
 	T.setIdentity();
@@ -39,28 +38,51 @@ int main(int argc, char** argv)
 	po::options_description desc("Allowed options");
     desc.add_options()
 	("help", "produce help message")
-	 ("trans", po::value<std::vector<string> >()->multitoken(), "transform files");
+	("invert", "apply Td*T instead of T*Td")
+	 ("trans", po::value<std::vector<string> >()->multitoken(), "Transform files");
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
     po::notify(vm);
 	vector<string> trans_files;
+	int is_pipe=1;
+	bool invert=false;
+	if(vm.count("invert"))
+		invert=true;
+	if(vm.count("help"))
+	{
+		cout<<desc;
+		cout<<"use a pipe if only one file with transforms"<<endl;
+		return 0;
+	}
 	if (!vm["trans"].empty() && (trans_files= vm["trans"].as<vector<string> >()).size() >= 2) {
+		is_pipe=0;
 		///cout<<"success pointcloud read";
-	}else {cout<<"transform read failure";};
+	}
 	int num_files=trans_files.size();
 	Eigen::Affine3d T;
 	T.setIdentity();
-	for(int i=0;i<num_files;i++)
+	for(int i=0;i<num_files+is_pipe;i++)
 	{
 		Eigen::Affine3d T_pred;
-		T_pred=getTrans(trans_files[i]);
-		T=T*T_pred;
-		for(int i=0;i<4;i++)
+		if(!is_pipe)
+		{
+			ifstream in_file(trans_files[i]);
+			T_pred=getTrans(in_file);
+		}
+		else
+		{
+			T_pred=getTrans(cin);
+			i--;
+		}
+		if(invert)
+			T=T_pred*T;
+		else
+			T=T*T_pred;
+		for(int k=0;k<4;k++)
 			for(int j=0;j<4;j++)
-				cout<<T(i,j)<<", ";
+				cout<<T(k,j)<<", ";
 		cout<<endl;
 	}
-
 	return 0;
 }
 
